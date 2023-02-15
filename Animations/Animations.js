4261ad813d4a38bcd4d5ba871b117f27770b21ff
Andrew Howell <118097175+AndrewHowell1@users.noreply.github.com>
2023-02-15 03:23:54 -0500

    Deleting useless JS

--- Animations/Animations.js
+++ Animations/Animations.js
@@ -1,21 +1,3 @@
-let section = document.querySelectorAll("section");
-let menu = document.querySelectorAll("header div a");
-window.onscroll = () => {
-  section.forEach((i) => {
-    let top = window.scrollY;
-    let offset = i.offsetTop - 150;
-    let height = i.offsetHeight;
-    let id = i.getAttribute("id");
-    if (top >= offset && top < offset + height) {
-      menu.forEach((link) => {
-        link.classList.remove("active");
-        document
-          .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active");
-      });
-    }
-  });
-};
 function reveal() {
   var reveals = document.querySelectorAll(".reveal");
 
